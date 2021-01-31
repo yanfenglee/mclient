57de0b59e1330687b258ef36ac05d051416199b8
yanfeng.li <yanfeng.li@yunqueyi.com>
2021-01-31 18:10:28 +0800

    update str util

--- src/str_utils.rs
+++ src/str_utils.rs
@@ -1,9 +1,8 @@
 
-use url::Url;
 use std::collections::HashMap;
 use regex::Regex;
 
-pub fn render_named(src: &str, vals: &HashMap<&str, &str>) -> String {
+pub fn replace_named(src: &str, vals: &HashMap<&str, &str>) -> String {
     let mut parts: Vec<&str> = vec![];
     let regex = Regex::new(r"\{([\w]+)\}").unwrap();
 
@@ -12,25 +11,16 @@ pub fn render_named(src: &str, vals: &HashMap<&str, &str>) -> String {
         .map(|m| (m.start(), m.end()))
         .collect();
 
-    println!("{:?}", matches);
-
     let mut offset = 0usize;
 
     for (start, end) in matches.iter() {
         parts.push(&src[offset..*start]);
-
-        // argument name with braces
         let arg = &src[*start..*end];
-        // just the argument name
         let arg_name = &src[*start+1..*end-1];
 
-        println!("arg name: {}", arg_name);
-
-        // if value passed for argument then append it, otherwise append original argument
-        // name with braces
         match vals.get(arg_name) {
             Some(s) => parts.push(s),
-            _ => parts.push(arg),
+            None => parts.push(arg),
         }
 
         offset = *end;
@@ -44,23 +34,16 @@ pub fn render_named(src: &str, vals: &HashMap<&str, &str>) -> String {
 }
 
 #[test]
-fn test_url_parse() {
-    let mut url: Url = Url::parse("http://localhost/{name}/{id}/").unwrap();
-    for seg in url.path_segments_mut() {
-        println!("{:?}", seg);
-    }
-
-    let path = url.path();
-    println!("path: {}", path);
-}
-
-#[test]
 fn test_replace() {
-    let path = "/path1/{name}/hello/{id}/resource";
     let mut data = HashMap::new();
     data.insert("name","lyf");
     data.insert("id","123");
 
-    let path = render_named(path, &data);
-    println!("{}", path)
+    assert_eq!(replace_named("/path1/{name}/hello/{id}/res", &data), "/path1/lyf/hello/123/res");
+
+    assert_eq!(replace_named("/{name}/{id}", &data), "/lyf/123");
+    assert_eq!(replace_named("/{name}/{id}/", &data), "/lyf/123/");
+    assert_eq!(replace_named("{name}/{id}/", &data), "lyf/123/");
+    assert_eq!(replace_named("{name2}/{id}/", &data), "{name2}/123/");
+    assert_eq!(replace_named("/path/res", &data), "/path/res");
 }
